68a6d3436f2c823d73e79bfe00397622108a4199
d-rez <dark.skeleton@gmail.com>
2019-01-09 01:10:06 +1100

    Overhaul (New devices, new Chroma visualisation adaptation, handling config options from the previous commit and housekeping
    Lots of changes:
    
    - Added and improved LIFX support, have as many bulbs as you want and don't let a bulb that dropped out of wifi impact visualisations
    - Added configurability in config.py for new devices
    - Secondary LED strip option
    - Moved definitions and variables around to make more sense
    - Cleaner, V2 of the Chroma visualiser where LED strip pixels are directly translated to peripherals. In this mode each peripheral displays separate section of the strip. Choose either new or old one in the config file
    - Some broken ctrl+c handling that doesn't work lol

--- python/led.py
+++ python/led.py
@@ -5,20 +5,61 @@ import platform
 import numpy as np
 import config
 from ChromaPython import ChromaApp, ChromaAppInfo, ChromaColor, Colors
+import lifxlan
+import colorsys
+import sys, signal
 
 # ESP8266 uses WiFi communication
 if config.DEVICE == 'esp8266':
     import socket
     _sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    Info = ChromaAppInfo
-    Info.DeveloperName = 'd-rez'
-    Info.DeveloperContact = 'dark.skeleton@gmail.com'
-    Info.Category = 'application'
-    Info.SupportedDevices = ['keyboard', 'mouse', 'mousepad', 'headset']
-    Info.Description = 'Oh Rick, I don\'t know if that\'s a good idea.'
-    Info.Title = 'Audio Reactive Chroma-extended LED strip'
 
-    App = ChromaApp(Info)
+    if config.DEVICES_ENABLED["CHROMA"]:
+        chroma_rate_counter = 0 # This counter help limit the FPS on lifx and chroma since otherwise they bug out
+
+        Info = ChromaAppInfo
+        Info.DeveloperName = 'd-rez'
+        Info.DeveloperContact = 'dark.skeleton@gmail.com'
+        Info.Category = 'application'
+        Info.SupportedDevices = ['keyboard', 'mouse', 'mousepad', 'headset', 'keypad']
+        Info.Description = 'Oh Rick, I don\'t know if that\'s a good idea.'
+        Info.Title = 'Audio Reactive Chroma-extended LED strip'
+        App = ChromaApp(Info)
+
+        KeyboardGrid = [[ChromaColor(red=0, green=0, blue=0) for x in range(App.Keyboard.MaxColumn)] for y in range(App.Keyboard.MaxRow)]
+        KeypadGrid = [[ChromaColor(red=0, green=0, blue=0) for x in range(App.Keypad.MaxColumn)] for y in range(App.Keypad.MaxRow)]
+        MousepadGrid = [ChromaColor(red=0, green=0, blue=0) for x in range(App.Mousepad.MaxLED)]
+
+    if config.DEVICES_ENABLED["LIFX"]:
+        lifx_rate_counter = 0 # This counter help limit the FPS on lifx and chroma since otherwise they bug out
+
+        try:
+            lifx = lifxlan.LifxLAN()
+            bulbs = [
+                lifx.get_device_by_name("TV light"),
+                lifx.get_device_by_name("Window light")]#,
+                #lifx.get_device_by_name("Ceiling")]
+            for bulb in bulbs:
+                bulb.set_power(True)
+        except: # Failed to init
+            config.DEVICES_ENABLED["LIFX"] = False
+
+    def signal_handler(signal, frame):
+        # This isn't working anyway yet for some reason ;/
+        if config.DEVICES_ENABLED["CHROMA"]:
+            App.Keyboard.setNone()
+            App.Keypad.setNone()
+            App.Mouse.setNone()
+            App.Mousepad.setNone()
+            App.Headset.setNone()
+            del App
+        if config.DEVICES_ENABLED["LIFX"]:
+            for bulb in bulbs:
+                bulb.set_power(False)
+        print("Exiting ayy")
+        sys.exit(0)
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
 
 # Raspberry Pi controls the LED strip directly
 elif config.DEVICE == 'pi':
@@ -51,11 +92,6 @@ _prev_pixels = np.tile(253, (3, config.N_PIXELS))
 pixels = np.tile(1, (3, config.N_PIXELS))
 """Pixel values for the LED strip"""
 
-KeyboardGrid = [[ChromaColor(red=0, green=0, blue=0) for x in range(App.Keyboard.MaxColumn)] for y in range(App.Keyboard.MaxRow)]
-KeypadGrid = [[ChromaColor(red=0, green=0, blue=0) for x in range(App.Keypad.MaxColumn)] for y in range(App.Keypad.MaxRow)]
-MousepadGrid = [ChromaColor(red=0, green=0, blue=0) for x in range(App.Mousepad.MaxLED)]
-
-chroma_rate_counter = 0
 _is_python_2 = int(platform.python_version_tuple()[0]) == 2
 
 def _update_esp8266():
@@ -96,13 +132,74 @@ def _update_esp8266():
                 m.append(p[2][i])  # Pixel blue value
         m = m if _is_python_2 else bytes(m)
         _sock.sendto(m, (config.UDP_IP, config.UDP_PORT))
+        if config.DEVICES_ENABLED["LED_STRIP2"]:
+            _sock.sendto(m, (config.UDP_IP2, config.UDP_PORT))
     _prev_pixels = np.copy(p)
+
+def _update_chroma_v2():
+    """
+    This function runs Chroma at full resolution without any rescaling.
+    Every device will display different section of the LED strip's spectrum
+    This is my new implementation but it doesn't have the side-strips working on mice that have it (yet)
+    """
+    global chroma_rate_counter
+    chroma_rate_counter = (chroma_rate_counter+1) % 2
+    if chroma_rate_counter != 1:
+      # Assuming 60FPS, we only want to drive Chroma at 30FPS - leave the function every other iteration. Otherwise weird things can happen
+      return
+    global pixels, _prev_pixels, KeyboardGrid, KeypadGrid, MousepadGrid
+
+    if config.CHROMA_TKL_KEYBOARD:
+      keyboard_columns = App.Keyboard.MaxColumn - 6 # This will "center" the effect better on laptops and TKLs
+    else:
+      keyboard_columns = App.Keyboard.MaxColumn
+
+    # Some calculations for array limits
+    keyb_l = int(config.N_PIXELS/2-keyboard_columns/2)
+    keyb_r = keyb_l + keyboard_columns
+    keyp_r = keyb_l - 1
+    keyp_l = keyp_r - App.Keypad.MaxColumn
+    mpad_l = keyb_r + 1
+    mpad_r = mpad_l + App.Mousepad.MaxLED
+
+    App.Headset.setNone()
+
+    # Keyboard
+    for x in range(keyb_l, keyb_r):
+      KeyboardGrid[0][x-keyb_l].set(red=int(pixels[0][x]), green=int(pixels[1][x]), blue=int(pixels[2][x]))
+    App.Keyboard.setCustomGrid(KeyboardGrid)
+    App.Keyboard.applyGrid()
+    KeyboardGrid.insert(0,[ChromaColor(red=0, green=0, blue=0) for x in range(App.Keyboard.MaxColumn)])
+    del KeyboardGrid[-1]
+
+    # Keypad
+    for x in range(keyp_l, keyp_r):
+      KeypadGrid[0][x-keyp_l].set(red=int(pixels[0][x]), green=int(pixels[1][x]), blue=int(pixels[2][x]))
+    App.Keypad.setCustomGrid(KeypadGrid)
+    App.Keypad.applyGrid()
+    KeypadGrid.insert(0,[ChromaColor(red=0, green=0, blue=0) for x in range(App.Keypad.MaxColumn)])
+    del KeypadGrid[-1]
+
+    # Mousepad
+    for x in range(mpad_l, mpad_r):
+      MousepadGrid[App.Mousepad.MaxLED-(x-mpad_l)-1].set(red=int(pixels[0][x]), green=int(pixels[1][x]), blue=int(pixels[2][x]))
+    App.Mousepad.setCustomGrid(MousepadGrid)
+    App.Mousepad.applyGrid()
+
+    # Mouse
+    App.Mouse.setPosition(x=3,y=2,color=ChromaColor(red=pixels[0][mpad_l], green=pixels[1][mpad_l], blue=pixels[2][mpad_l]))
+    App.Mouse.setPosition(x=3,y=7,color=ChromaColor(red=pixels[0][mpad_l+1], green=pixels[1][mpad_l+1], blue=pixels[2][mpad_l+1]))
+    App.Mouse.applyGrid()
+
+    # Headset
+    App.Headset.setNone()
+    App.Headset.setStatic(color=ChromaColor(red=pixels[0][mpad_l],green=pixels[1][mpad_l],blue=pixels[2][mpad_l]))
 
 def _update_chroma_scaled():
     """
     This function runs Chroma at scaled resolution.
     Every device will display the exact same section of the LED strip's spectrum, scaled down to each device's size
-    This is my old implementation
+    This is my old implementation, but it has the side-strips working on mice that have it
     """
     global chroma_rate_counter, KeyboardGrid
     chroma_rate_counter = (chroma_rate_counter+1) % 2
@@ -154,6 +251,26 @@ def _update_chroma_scaled():
     KeypadGrid.insert(0,[ChromaColor(red=0, green=0, blue=0) for x in range(App.Keypad.MaxColumn)])
     del KeypadGrid[-1]
 
+def _update_lifx():
+    global lifx_rate_counter
+    lifx_rate_counter = lifx_rate_counter + 1
+    global pixels, _prev_pixels
+
+    if lifx_rate_counter % 4 != 0:
+      return
+
+    # Convert to LIFX-friendly format
+    l1_hls = colorsys.rgb_to_hls(pixels[0][int(config.N_PIXELS/2)]/255.0,pixels[1][int(config.N_PIXELS/2)]/255.0,pixels[2][int(config.N_PIXELS/2)]/255.0)
+    l1 = [int(l1_hls[0]*65535), int(l1_hls[2]*65535), int(l1_hls[1]*65535), 3500]
+
+    for bulb in bulbs:
+        bulb.set_color(l1, 25, True)
+
+    # Uncomment this and play with it if you want your bulbs show different sections of the strip
+    #l2_hls = colorsys.rgb_to_hls(moving_avg[1][0]/3,moving_avg[1][1]/3,moving_avg[1][2]/3)
+    #l2_hls = colorsys.rgb_to_hls(pixels[0][0]/255.0,pixels[1][0]/255.0,pixels[2][0]/255.0)
+    #l2 = [int(l2_hls[0]*65535), int(l2_hls[2]*65535), int(l2_hls[1]*65535), 3500]
+    #lc.set_color(l2, 25, True)
 
 def _update_pi():
     """Writes new LED values to the Raspberry Pi's LED strip
@@ -185,7 +302,7 @@ def _update_blinkstick():
         This function updates the LED strip with new values.
     """
     global pixels
-    
+
     # Truncate values and cast to integer
     pixels = np.clip(pixels, 0, 255).astype(int)
     # Optional gamma correction
@@ -211,14 +328,19 @@ def update():
     """Updates the LED strip values"""
     if config.DEVICE == 'esp8266':
         _update_esp8266()
-        _update_chroma()
+        if config.DEVICES_ENABLED["CHROMA"]:
+            if config.CHROMA_VISTYPE_SCALED:
+                _update_chroma_scaled()
+            else:
+                _update_chroma_v2()
+        if config.DEVICES_ENABLED["LIFX"]:
+            _update_lifx()
     elif config.DEVICE == 'pi':
         _update_pi()
     elif config.DEVICE == 'blinkstick':
         _update_blinkstick()
     else:
         raise ValueError('Invalid device selected')
-
 
 # Execute this file to run a LED strand test
 # If everything is working, you should see a red, green, and blue pixel scroll
